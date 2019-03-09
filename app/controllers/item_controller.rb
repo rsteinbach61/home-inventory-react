1fb96c179156c100b11b01ab1e0372481364506e
Richard Steinbach <steinbach.dick@gmail.com>
2019-03-08 22:13:34 -0800

    flesh out item controller

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,2 +1,24 @@
 class ItemController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(name: params[:itemname], room_id: params[:roomId])
+    render json: @item
+  end
+
+  def index
+    @items = Item.all
+    render json: @items
+  end
+
+  def delete
+    @item = Item.find_by(id: params[:_json])
+    @item.destroy
+    @items = Items.all
+    render json: @items
+  end
+
 end

106f6d75e42d0c6ffd4eadbecbf18ef1f897f419
Richard Steinbach <steinbach.dick@gmail.com>
2019-03-21 14:06:21 -0700

    add labels to item display

--- client/src/components/items/Item.js
+++ client/src/components/items/Item.js
@@ -7,9 +7,14 @@ class Item extends Component {
     return(
       <div>
         <h3>Item: </h3>
-        <p>{this.props.item.name}</p>
-        <p>{this.props.item.make}</p>
-        <p>{this.props.item.model}</p>
+        <p>Name: {this.props.item.name}</p>
+        <p>Make: {this.props.item.make}</p>
+        <p>Model: {this.props.item.model}</p>
+        <p>UPC: {this.props.item.upc}</p>
+        <p>Purchase Date: {this.props.item.purchase_date}</p>
+        <p>Price: {this.props.item.purchase_price}</p>
+        <p>Serial Num: {this.props.item.serial_num}</p>
+
       </div>
     )
   }

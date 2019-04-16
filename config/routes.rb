1d35114922ce1af4b65a0535aa02821149c7681d
Richard Steinbach <steinbach.dick@gmail.com>
2019-04-16 10:16:24 -0700

    clean up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api/' do
-  #  get :home, to: 'home#index'
-  #  post :create, to: 'home#create'
-  #  post :delete, to: 'home#delete'
-    get :rooms, to: 'room#index'
     get 'home/:id/rooms', to: 'home#rooms'
     get 'room/:id/items', to: 'room#items'
     resources :home
